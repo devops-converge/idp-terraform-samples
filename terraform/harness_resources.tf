2dacafb623b261116e97d3b3bf62854e631f70d3
vrathinam1984 <115575958+vrathinam1984@users.noreply.github.com>
2024-08-23 14:02:11 -0400

    Update harness_resources.tf

--- terraform/harness_resources.tf
+++ terraform/harness_resources.tf
@@ -1,37 +1,13 @@
-resource "harness_platform_pipeline" "pipeline" {
-  identifier = "idp_create_resource_pipeline"
-  org_id     = "default"
-  name = "idp create resource pipeline"
-  project_id = "testvictor"
-  yaml = <<-EOT
-    pipeline:
-      name: idp create resource pipeline
-      identifier: idp_create_resource_pipeline
-      projectIdentifier: testvictor
-      orgIdentifier: default
-      stages:
-        - stage:
-            name: idp
-            identifier: idp
-            description: ""
-            type: IDP
-            spec:
-              platform:
-                os: Linux
-                arch: Amd64
-              runtime:
-                type: Cloud
-                spec: {}
-              execution:
-                steps:
-                  - step:
-                      type: Run
-                      name: Run_1
-                      identifier: Run_1
-                      description: test hello
-                      spec:
-                        shell: Sh
-                        command: aws s3api create-bucket --bucket test-victor-my-bucket --region us-east-1
-            tags: {}
-  EOT
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "your-unique-bucket-name-harness"
+  acl    = "private"
+
+  tags = {
+    Name        = "HarnessDemoBucket"
+    Environment = "Dev"
+  }
 }
